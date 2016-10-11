7484bd5f486033a87cc8a37d68b2ac7fe41a6a15
ffuertes <felipe.fuertes@gmail.com>
2016-10-11 09:47:52 -0400

    Languages Fixes and Grid
    Added a quick grid system and media query mixin.

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,9 +11,9 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function boilerplate_customize_register( $wp_customize ) {
-	$wp_customize->getboilerplateetting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->getboilerplateetting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->getboilerplateetting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 }
 add_action( 'customize_register', 'boilerplate_customize_register' );
 
